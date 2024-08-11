4509e0cf85e1dc7ff6ebb9031aad8cd3bf1ed3ec
Putra Chandra <it@putrachandra.com>
2024-08-11 23:35:34 +0700

    Final Project React Styling

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { FaCoffee } from "react-icons/fa";
+import Logo from "../../assets/website/coffee_logo.png";
+const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Services",
+    link: "/#services",
+  },
+  {
+    id: 3,
+    name: "About",
+    link: "/#about",
+  },
+];
+
+const Navbar = ({ menuItems, logo }) => {
+  return (
+    <>
+      <div className="bg-gradient-to-r from-secondary to-secondary/90 shadow-md bg-gray-900 text-white">
+        <div className="container py-2">
+          <div className="flex justify-between items-center">
+            {/* Logo section */}
+            <div data-aos="fade-down" data-aos-once="true">
+              <a
+                href="#"
+                className="font-bold text-2xl sm:text-3xl flex justify-center items-center gap-2 tracking-wider font-cursive"
+              >
+                <img src={logo} alt="Logo" className="w-14" />
+                Uno Cafe
+              </a>
+            </div>
+
+            {/* Link section */}
+            <div
+              data-aos="fade-down"
+              data-aos-once="true"
+              data-aos-delay="300"
+              className="flex justify-between items-center gap-4"
+            >
+              <ul className="hidden sm:flex items-center gap-4">
+                {menuItems.map((item) => (
+                  <li key={item.id}>
+                    <a
+                      href={item.link}
+                      className="inline-block text-xl py-4 px-4 text-white/70 hover:text-white duration-200"
+                    >
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+              <button className="bg-primary/70 hover:scale-105 duration-200 text-white px-4 py-2 rounded-full flex items-center gap-3">
+                Order
+                <FaCoffee className="text-xl text-white drop-shadow-sm cursor-pointer" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar menuItems={Menu} logo={Logo} />
+    </div>
+  );
+};
+
+export default App;
